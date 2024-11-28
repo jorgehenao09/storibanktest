59248ed8ff727a3e56914c925162a514c6f4ebbc
Jorge Henao <jhneao@merqueo.com>
2024-11-28 18:28:50 -0500

    [settings]: First commit

---
+++ app/src/main/java/com/jhtest/storibanktest/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jhtest.storibanktest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jhtest.storibanktest.ui.theme.StoriBankTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StoriBankTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StoriBankTTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/jhtest/storibanktest/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.jhtest.storibanktest.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val PrimaryColor = Color(0xFF003B40)
+val SecondaryColor = Color(0xFF00D17F)
+val TertiaryColor = Color(0xFFD32F2F)
+val BackgroundColor = Color(0xFFFFFBFE)
