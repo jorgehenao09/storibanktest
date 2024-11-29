7472a068938a1399979b0e1311cb07a5e6d5aa78
Jorge Henao <jhneao@merqueo.com>
2024-11-29 17:37:07 -0500

    [feat]: DataStore was added on the project and UserPreferences.proto has been created to manage user information

---
+++ app/src/main/java/com/jhtest/storibanktest/data/repository/datasources/UserPreferencesProtoDataStore.kt
@@ -0,0 +1,13 @@
+package com.jhtest.storibanktest.data.repository.datasources
+
+import com.jhtest.storibanktest.data.datastore.UserPreferencesProto
+import kotlinx.coroutines.flow.Flow
+
+interface UserPreferencesProtoDataStore {
+
+    val userDataProto: Flow<UserPreferencesProto>
+
+    suspend fun saveUserData(userPreferences: UserPreferencesProto)
+
+    suspend fun clear()
+}
