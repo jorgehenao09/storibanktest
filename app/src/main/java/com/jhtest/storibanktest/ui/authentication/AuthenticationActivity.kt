f399b3c94dee4d78d67b0b698753cc2a62f5204f
Jorge Henao <jhneao@merqueo.com>
2024-11-29 11:58:51 -0500

    [feat]: AuthenticationActivity is the initial activity to handle all authentication process

--- app/src/main/java/com/jhtest/storibanktest/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/jhtest/storibanktest/ui/authentication/AuthenticationActivity.kt
@@ -1,4 +1,4 @@
-package com.jhtest.storibanktest
+package com.jhtest.storibanktest.ui.authentication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,42 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jhtest.storibanktest.ui.theme.StoriBankTTheme
 
-class MainActivity : ComponentActivity() {
+class AuthenticationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             StoriBankTTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StoriBankTTheme {
-        Greeting("Android")
     }
 }
