601575edf65eb9afaa9503a2506a5b394fc34b16
Jorge Henao <jhneao@merqueo.com>
2024-12-04 01:06:32 -0500

    [feat]: Unit test to Logi flow

--- app/src/main/java/com/jhtest/storibanktest/ui/home/HomeActivity.kt
+++ app/src/main/java/com/jhtest/storibanktest/ui/home/HomeActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 class HomeActivity : ComponentActivity() {
@@ -40,8 +43,14 @@ class HomeActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "ESTAMOS EN CONSTRUCCION!",
-        modifier = modifier
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "ESTAMOS EN CONSTRUCCION!",
+            modifier = modifier
+        )
+    }
 }
